22ad69227e1c4e6e031128527b4d13669b539d10
CarlOscarMarcus <oscar@wysarna.se>
2026-06-28 22:40:43 +0200

    Fix: pipeline error

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,25 @@
-import { Routes, Route } from "react-router-dom"
-import Layout from "../components/layout/Layout"
+import { Routes, Route } from "react-router-dom";
+import Layout from "../components/layout/Layout";
 
-import Home from "../pages/Home/Home"
-import About from "../pages/About/About"
-import Projects from "../pages/Projects/Projects"
-import Education from "../pages/Education/Education"
-import Contact from "../pages/Contact/Contact"
-import CV from "../pages/CV/CV"
+import Home from "../pages/Home/Home.jsx";
+import About from "../pages/About/About.jsx";
+import Projects from "../pages/Projects/Projects.jsx";
+import Education from "../pages/Education/Education.jsx";
+import Contact from "../pages/Contact/Contact.jsx";
+import CV from "../pages/CV/CV.jsx";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cv" element={<CV />} />
-      </Route>
-    </Routes>
-  )
+	return (
+		<Routes>
+			<Route element={<Layout />}>
+				<Route path="/" element={<Home />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/education" element={<Education />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/cv" element={<CV />} />
+			</Route>
+		</Routes>
+	);
 }
